fc35b767f5a2de32f1ab6e10cafc7d05b98c2a86
Oscar Tran <oscartran007@gmail.com>
2025-09-03 01:23:33 -0700

    Adjust squares styles to appear in css file Moved some of the style parameters to the css files that were universal

--- javascript.js
+++ javascript.js
@@ -1,26 +1,24 @@
 let INITIAL_ROWS = 16, INITIAL_COLUMNS = 16
-let BORDER_SETTINGS = "3px black solid"
+let BORDER_SETTINGS = "2px black solid"
 
 
 const container = document.querySelector(".container")
 
-let createGrid = function(rows, cols){
+function createGrid(rows, cols){
+    //Ratio of size for the individual squares
+    let sizeRatio = 100.0/rows
     for(let curSquare = 0; curSquare < rows*cols; curSquare ++){
         let square = document.createElement("div")
-        let sizeRatio = 100.0/rows
-        container.appendChild(square)
-        square.style.aspectRatio = " 1 / 1"
+        square.className = "square"
         square.style.width = `${sizeRatio}%`
-        square.style.boxSizing = "border-box"
-        //Settings to prevents the borders of extending
+        //Settings to prevent the borders from extending
         if (curSquare/cols < 1){
             square.style.borderTop = BORDER_SETTINGS
         }
         if (curSquare%cols === 0){
             square.style.borderLeft = BORDER_SETTINGS
         }
-        square.style.borderRight = BORDER_SETTINGS
-        square.style.borderBottom = BORDER_SETTINGS
+        container.appendChild(square)
     }
 }
 
